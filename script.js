6c5bfbfbf4fc94ea9756e5aeede09f91eb9cb676
Daria Viter <viter.daria@gmail.com>
2023-05-07 14:04:59 +0100

    fixes time/weather

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 //show date, time
+handleCurrentPosition();
 
-let now = new Date();
 let weekDays = [
   "Sunday",
   "Monday",
@@ -10,19 +10,21 @@ let weekDays = [
   "Friday",
   "Saturday",
 ];
-let date = now.getDate();
-let hours = now.getHours();
-let minutes = now.getMinutes();
-let day = weekDays[now.getDay()];
-
-let time = document.querySelector(".right-undertitle");
-time.innerHTML = `${day}, ${hours}:${minutes}`;
 
 // change C to F
 
 let fahrenheit = document.querySelector(".fahrenheit");
 let celsius = document.querySelector(".celsius");
 let temp = document.querySelector(".temp");
+let date = document.querySelector(".date");
+
+function formatDate(timestamp) {
+  let now = new Date(timestamp);
+  let hours = now.getHours();
+  let minutes = now.getMinutes();
+  let day = weekDays[now.getDay()];
+  return `${day} ${hours}:${(minutes < 10 ? "0" : "") + minutes}`;
+}
 
 fahrenheit.addEventListener("click", changeToFahrenheit);
 celsius.addEventListener("click", changeToCelsius);
@@ -40,24 +42,17 @@ function changeToCelsius() {
   temp.innerHTML = celsius;
 }
 
-//
-// let city = prompt("Enter the city:").toLowerCase();
-// lookForCity(city);
-
-// function lookForCity(city) {
-//   // const capitalCity = city.charAt(0).toUpperCase() + city.slice(1);
-// }
-
-// alert(
-//   `Sorry, we don't know the weather for this city, try going to https://www.google.com/search?q=weather+${city}`
-// );
+//currentPosition
 
 let key = "16edd4f908ec4dacf218c521821a8b8f";
+
 let requestedWeather = document.querySelector(".btn-success");
 let currentWeather = document.querySelector(".btn-primary");
 
 let city = document.querySelector("#search-text-input");
 let cityRightSide = document.querySelector(".right-title");
+let humidity = document.querySelector("#humidity");
+let wind = document.querySelector("#wind");
 
 currentWeather.addEventListener("click", handleCurrentPosition);
 requestedWeather.addEventListener("click", handleWeatherRequest);
@@ -66,31 +61,45 @@ function handleCurrentPosition() {
   navigator.geolocation.getCurrentPosition(requestCurrentTemp);
 }
 
+
+
 function requestCurrentTemp(position) {
   axios
     .get(
-      `https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${key}`
+      `https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${key}&units=metric`
     )
     .then(showCurrentTemp);
 }
 
 function showCurrentTemp(response) {
-  console.log(response.data);
-  let tempCurrent = Math.round(response.data.main.temp - 273.15);
-  temp.innerHTML = tempCurrent;
-  cityRightSide.innerHTML = response.data.name;
+  if (response) {
+    let tempCurrent = Math.round(response.data.main.temp);
+    temp.innerHTML = tempCurrent;
+    humidity.innerHTML = `Humidity: ${response.data.main.humidity} %`;
+    wind.innerHTML = `Wind: ${Math.round(response.data.wind.speed)} m/s`;
+    // date.innerHTML = `${day}, ${hours}:${(minutes < 10 ? "0" : "") + minutes}`;
+    date.innerHTML = formatDate(response.data.dt * 1000);
+
+    cityRightSide.innerHTML = response.data.name;
+  }
 }
+
+//requestedData
 
 function handleWeatherRequest() {
   event.preventDefault();
 
-  city.value.charAt(0).toUpperCase();
-  cityRightSide.innerHTML = city.value;
-  axios
-    .get(
-      `https://api.openweathermap.org/data/2.5/weather?q=${city.value}&appid=${key}&units=metric`
-    )
-    .then(showRequestedCityTemp);
+  if (city.value) {
+    city.value.charAt(0).toUpperCase();
+    cityUpperCase = city.value.charAt(0).toUpperCase() + city.value.slice(1);
+    cityRightSide.innerHTML = cityUpperCase;
+
+    axios
+      .get(
+        `https://api.openweathermap.org/data/2.5/weather?q=${city.value}&appid=${key}&units=metric`
+      )
+      .then(showRequestedCityTemp);
+  }
 }
 
 function showRequestedCityTemp(response) {
